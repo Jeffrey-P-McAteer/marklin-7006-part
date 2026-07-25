4485922ca1220bd8e76740ddc5da01c6e82827b7
Jeffrey McAteer <jeffrey@jmcateer.com>
2026-07-25 10:08:08 -0400

    Added first two boxes to cut out cylinder

--- part-design.py
+++ part-design.py
@@ -15,16 +15,114 @@ except:
     traceback.print_exc()
     repo_root = '/j/proj/marklin-7006-part'
 
-# Create a 40x30x10 mm block
-part = (
+# Store reference design part design measurements here, we can adjust these and
+# use equations below to calculate all dependent transforms.
+# All units in MM
+
+inner_length = 7.7
+inner_width = 3.0 # 3.07 # Jeff's adjusting this because it makes boolean geometric operations a pain in the butt - let me know if we really need a 0.07mm lip.
+base_height = 1.0
+cylinder_radius = 1.5
+cylinder_extension_height = 3.0
+cylinder_lcut_base_inset = 0.85   # Section in line w/ base, distance from edge in towards center
+cylinder_lcut_raised_inset = cylinder_lcut_base_inset + 1.3 # Section above the base
+cylinder_lcut_width = 0.79 # Width of the lcut, centered on the cylinder
+
+# Computed lengths for simpler equations downstairs
+part_total_length = inner_length + (2.0*cylinder_radius)
+cylinder_total_height = base_height + cylinder_extension_height
+
+# Create rectangle
+base = (
     cq.Workplane("XY")
-    .box(40, 30, 10)
-    .faces(">Z")
-    .workplane()
-    .hole(5)  # 5 mm diameter through-hole
+    .box(
+        inner_length,
+        inner_width,
+        base_height
+    )
 )
 
+# End cylinders
+left_cylinder = (
+    cq.Workplane("XY")
+    .cylinder(
+        cylinder_total_height,
+        cylinder_radius
+    )
+    .translate(
+        (
+            (-(inner_length) / 2.0),
+            0,
+            (cylinder_total_height / 2.0) - (base_height/2.0)
+        )
+    )
+)
 
+right_cylinder = (
+    cq.Workplane("XY")
+    .cylinder(
+        cylinder_total_height,
+        cylinder_radius
+    )
+    .translate(
+        (
+            ((inner_length) / 2.0),
+            0,
+            (cylinder_total_height / 2.0) - (base_height/2.0)
+        )
+    )
+)
+
+part = (
+    base
+    .union(left_cylinder)
+    .union(right_cylinder)
+)
+
+# part is now the base plus our cylinders. We next need to design two boxes to intersect and cut out the
+# desired empty space on both ends of the part.
+# We compute (part_total_length/2.0) to use as the distance from the center to begin the boxes at.
+box_center_offset = part_total_length / 2.0
+cutters = []
+
+top_l_box_positive = (
+    cq.Workplane("XY")
+    .box(
+        cylinder_lcut_raised_inset * 2.0,
+        cylinder_lcut_width,
+        cylinder_extension_height
+    )
+    .translate(
+        (
+            ((part_total_length) / 2.0),
+            0,
+            cylinder_total_height / 2.0
+        )
+    )
+)
+cutters.append(top_l_box_positive)
+
+top_l_box_negative = (
+    cq.Workplane("XY")
+    .box(
+        cylinder_lcut_raised_inset * 2.0,
+        cylinder_lcut_width,
+        cylinder_extension_height
+    )
+    .translate(
+        (
+            -((part_total_length) / 2.0),
+            0,
+            cylinder_total_height / 2.0
+        )
+    )
+)
+cutters.append(top_l_box_negative)
+
+
+# Now we slice the computex boxes off one at a time
+for cutter in cutters:
+    part = part.cut(cutter)
 
 # Export as STL
 os.makedirs(os.path.join(repo_root, 'build'), exist_ok=True)
